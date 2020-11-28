cd252e5176dad39eac733ab54e2495f845b1f134
Valerii Stefanyk <valeriistefanyk@gmail.com>
2020-11-29 00:44:42 +0200

    New: added new book creating method & template form

--- bookshop/blueprints/books.py
+++ bookshop/blueprints/books.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request, url_for
 from bookshop.models import Book
-
+from bookshop.extenstions import db
 
 books = Blueprint('books', __name__)
 
@@ -14,6 +14,15 @@ def index():
 def details(book_id):
     book = Book.query.get_or_404(book_id)
     return render_template('books/detail.html', book=book)
+
+@books.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        book = Book(title=request.form['title'], description=request.form['description'])
+        db.session.add(book)
+        db.session.commit()
+        return redirect(url_for('books.details', book_id=book.id))
+    return render_template('books/create.html')
 
 @books.errorhandler(404)
 def not_found(exception):
