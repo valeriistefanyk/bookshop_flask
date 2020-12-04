d4332786611555189001893d85cfb07b36ce0e45
Valerii Stefanyk <valeriistefanyk@gmail.com>
2020-12-04 21:36:49 +0200

    Added translate init language command to flask cli

--- bookshop/commands/translate.py
+++ bookshop/commands/translate.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+import click
 import os
 
 translate_command = Blueprint('translate', __name__, 
@@ -19,3 +20,13 @@ def compile():
     """Compile all languages."""
     if os.system('pybabel compile -d bookshop/translations'):
         raise RuntimeError('compile command failed')
+
+@translate_command.cli.command('init')
+@click.argument('lang')
+def init(lang):
+    """Initialize a new language."""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot ./bookshop/'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel init -i messages.pot -d bookshop/translations -l ' + lang):
+        raise RuntimeError('init command failed')
+    os.remove('./messages.pot')
